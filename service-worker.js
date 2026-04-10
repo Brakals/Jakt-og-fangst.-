540423baee3339072ac84d9b42d9048f2b3724e6
Brakals <semann93@hotmail.com>
2026-04-10 03:55:53 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+{
+  "name": "Jakt App",
+  "short_name": "Jakt",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": []
+}
